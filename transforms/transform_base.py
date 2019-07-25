966d62b024945a2f26b6d02419a25aa27b2d814b
Bo <spare@Landing-JJHC9.local>
2019-07-24 19:16:48 -0700

    minor fix

--- transforms/transform_base.py
+++ transforms/transform_base.py
@@ -1,11 +1,5 @@
-import numpy as np
-
-
 class TransformBase:
     def __init__(self, config):
-        # transform[0]: Transform only image
-        # transform[1]: Transform both image and label
-        # transform[2]: Transform only label
         self.transform = {
             "IMAGE_ONLY": None,
             "IMAGE_LABEL": None,
@@ -13,22 +7,16 @@ class TransformBase:
         }
         self.config = config
 
-    def apply_transforms(self, image, segmentation_labels):
-        image = image
-        seg_label = segmentation_labels
+    def apply_transforms(self, image, label):
         if self.transform["IMAGE_ONLY"] is not None:
             image = self.transform["IMAGE_ONLY"](image=image)["image"]
         if self.transform["IMAGE_LABEL"] is not None:
-            augmented = self.transform["IMAGE_LABEL"](image=image,
-                                                      mask=seg_label)
+            augmented = self.transform["IMAGE_LABEL"](image=image, mask=label)
             image = augmented["image"]
-            seg_label = augmented["mask"]
+            label = augmented["mask"]
         if self.transform["LABEL_ONLY"] is not None:
-            seg_label = self.transform["LABEL_ONLY"](mask=seg_label)["mask"]
-
-        image = image
-        seg_label = seg_label
-        return [image, seg_label]
+            label = self.transform["LABEL_ONLY"](mask=label)["mask"]
+        return [image, label]
 
     def has_transform(self):
         if self.transform is not None and \
