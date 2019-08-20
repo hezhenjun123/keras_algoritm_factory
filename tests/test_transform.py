cc588beb78553c70181eeef226cc9a47389cb3b9
Beomsik <beomsik@landing.ai>
2019-08-20 15:08:48 -0700

    support apply mixed type of transforms with unit tests

---
+++ tests/test_transform.py
@@ -0,0 +1,74 @@
+from transforms.transform_base import TransformBase
+
+#helper functions
+def transform_plus_1():
+    def func(image=None, mask=None):
+        if image:
+            image[0] = image[0] + 1
+        if mask:
+            mask[0] = mask[0] + 1
+        return {"image":image, "mask":mask}
+    return func
+
+def transform_mul_2():
+    def func(image=None, mask=None):
+        if image:
+            image[0] = image[0] * 2
+        if mask:
+            mask[0] = mask[0] * 2
+        return {"image":image, "mask":mask}
+    return func
+
+#tests begin here
+def test_transform_image():
+    image = [0,0,0]
+    label = [1,1,1]
+    augmented = TransformBase.Image(transform_plus_1())(image, label)
+    assert len(augmented.keys()) == 2
+    assert augmented["image"] == [1,0,0]
+    assert augmented["mask"] == [1,1,1]
+
+def test_transform_image_label():
+    image = [0,0,0]
+    label = [1,1,1]
+    augmented = TransformBase.ImageLabel(transform_plus_1())(image, label)
+    assert len(augmented.keys()) == 2
+    assert augmented["image"] == [1,0,0]
+    assert augmented["mask"] == [2,1,1]
+
+def test_transform_label():
+    image = [0,0,0]
+    label = [1,1,1]
+    augmented = TransformBase.Label(transform_plus_1())(image, label)
+    assert len(augmented.keys()) == 2
+    assert augmented["image"] == [0,0,0]
+    assert augmented["mask"] == [2,1,1]
+
+
+def test_apply_transform():
+    transform = TransformBase({})
+    transform.transforms = [
+        TransformBase.Image(transform_plus_1()),
+        TransformBase.ImageLabel(transform_plus_1()),
+        TransformBase.Label(transform_mul_2()),
+        #Image should be applied after the above transforms.
+        TransformBase.Image(transform_mul_2()),
+    ]
+
+    image = [10,10,10]
+    label = [11,11,11]
+    augmented = transform.apply_transforms(image, label)
+    assert augmented[0] == [24,10,10]
+    assert augmented[1] == [24,11,11]
+
+def test_backward_compatibilty():
+    transform = TransformBase({})
+    transform.transform["IMAGE_ONLY"] = [transform_plus_1(), transform_mul_2()]
+    transform.transform["IMAGE_LABEL"] = [transform_plus_1()]
+    transform.transform["LABEL_ONLY"] = [transform_mul_2(), transform_plus_1()]
+
+    image = [10,10,10]
+    label = [11,11,11]
+    augmented = transform.apply_transforms(image, label)
+    assert augmented[0] == [23,10,10]
+    assert augmented[1] == [25,11,11]
--- transforms/transform_base.py
+++ transforms/transform_base.py
@@ -1,10 +1,49 @@
 class TransformBase:
+    class Image:
+        def __init__(self, transform):
+            self.transform = transform
+        def __call__(self, image, mask):
+            augmented = self.transform(image=image)
+            return {"image":augmented["image"], "mask":mask}
+
+    class ImageLabel:
+        def __init__(self, transform):
+            self.transform = transform
+        def __call__(self, image, mask):
+            augmented = self.transform(image=image, mask=mask)
+            return {"image":augmented["image"], "mask":augmented["mask"]}
+
+    class Label:
+        def __init__(self, transform):
+            self.transform = transform
+        def __call__(self, image, mask):
+            augmented = self.transform(mask=mask)
+            return {"image":image, "mask":augmented["mask"]}
 
     def __init__(self, config):
-        self.transform = {"IMAGE_ONLY": None, "IMAGE_LABEL": None, "LABEL_ONLY": None}
         self.config = config
+        self.transforms = []
+
+        #FIXME: remove this once all the transform classes migrated to use self.transforms.
+        self.transform = {"IMAGE_ONLY": None, "IMAGE_LABEL": None, "LABEL_ONLY": None}
 
     def apply_transforms(self, image, label):
+        if self.transforms:
+            for t in self.transforms:
+                augmented = t(image=image, mask=label)
+                image = augmented["image"]
+                label = augmented["mask"]
+            return [image, label]
+        else:
+            return self._apply_transforms_deprecated(image, label)
+
+    def has_transform(self):
+        if self._has_transform_deprecated():
+            return True
+
+        return len(self.transforms) > 0
+
+    def _apply_transforms_deprecated(self, image, label):
         if self.transform["IMAGE_ONLY"] is not None:
             for transform_func in self.transform["IMAGE_ONLY"]:
                 image = transform_func(image=image)["image"]
@@ -18,7 +57,7 @@ class TransformBase:
                 label = transform_func(mask=label)["mask"]
         return [image, label]
 
-    def has_transform(self):
+    def _has_transform_deprecated(self):
         if self.transform is not None and \
                 (self.transform["IMAGE_ONLY"] is not None or
                  self.transform["IMAGE_LABEL"] is not None or
