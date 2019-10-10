b3c73239dc9efbf6b58ace7cfca8d13de5936c98
Tim Rosenflanz <trosenflanz@gmail.com>
2019-10-10 13:05:19 -0700

    adhoc: extract timestamps  (#41)

--- adhoc/h5_to_avi.py
+++ adhoc/h5_to_avi.py
@@ -3,35 +3,43 @@ import os
 import cv2
 import argparse
 import multiprocessing as mp
-
+import shutil
 
 def h5_to_png(file_path):
     base_path, file_name = os.path.split(file_path)
-    save_path = base_path.replace("/uvc/", "/avi/")
-
-    if os.path.exists(save_path) is not True:
-        os.makedirs(save_path)
-        print('mkdir:%s' % save_path)
+    #h5 doesn't support reading from s3 mounted bucket
+    # so we copy the file
+    shutil.copyfile(file_path,file_name)
+    save_path_avi = base_path.replace("/uvc/", "/avi/")
+    save_path_h5 = base_path.replace("/uvc/", "/avi_frame_timestamps/")
+    for save_path in [save_path_avi,save_path_h5]:
+        if os.path.exists(save_path) is not True:
+            os.makedirs(save_path)
+            print('mkdir:%s' % save_path)
 
     try:
-        f = h5py.File(file_path, 'r')
-        timestamps = f['timestamp'][:]
+        f = h5py.File(file_name, 'r')
         data = f['data'][:]
         writer = None
-        for timestamp, img in zip(timestamps, data):
+        for img in data:
             # cv2.imwrite(os.path.join(save_path, str(timestamp)+'.png'), img)
             if writer is None:
                 fourcc = cv2.VideoWriter_fourcc(*'XVID')
                 video_shape = (img.shape[1], img.shape[0])
-                writer = cv2.VideoWriter(os.path.join(save_path, f"{file_name}.avi"), fourcc, 2,
+                writer = cv2.VideoWriter(os.path.join(save_path_avi, f"{file_name}.avi"), fourcc, 2,
                                          video_shape, True)
             writer.write(img)
         writer.release()
+        f_new = h5py.File(f"timestamps_{file_name}",'w-')
+        f.copy('timestamp',f_new)
+        f_new.close()
         f.close()
+        shutil.move(f"timestamps_{file_name}",os.path.join(save_path_h5, f"{file_name}"))
         print('Convert %s Done.' % file_path)
     except Exception as e:
         print(e)
     finally:
+        os.remove(file_name)
         pass
 
 
@@ -52,6 +60,7 @@ if __name__ == '__main__':
         pool = mp.Pool(processes=16)
 
         for root, dirs, files in os.walk(dir_path):
+            if 'avi_frame_timestamps' in root: continue
             for file in files:
                 if os.path.splitext(file)[-1] in ['.h5']:
                     file_path = os.path.join(root, file)
