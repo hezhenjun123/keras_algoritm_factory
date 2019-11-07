0382a8a8f1d8f4adf1f2ccbdbab934d19aa8d071
Tim Rosenflanz <trosenflanz@gmail.com>
2019-11-06 16:05:08 -0800

    new videos for yield estimation changes(#44)

--- submit_job.py
+++ submit_job.py
@@ -10,7 +10,7 @@ CONFIG_FILES = ['model_config_segmentation_lodging.yaml']
 def run_gru_job(config_path,gru_settings):
     gru.submit(job_name=gru_settings["JOB_NAME"],
                project_name=gru_settings["PROJECT_NAME"],
-               job_decription=f'GRU RUN {config_path}',
+               job_description=f'GRU RUN {config_path}',
                cmd=f'run.sh {config_path}',
                instance_type=gru_settings["INSTANCE_TYPE"],
                timeout_hour=gru_settings["TIMEOUT"])  # p2.xlarge, p3.2xlarge
