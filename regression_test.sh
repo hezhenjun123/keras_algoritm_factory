d5532f33c12f97d6f9ca3283bfd753d06b5fee76
Yuanzhe Dong <yuanzhedong@gmail.com>
2020-01-28 22:02:40 -0800

    update

--- regression_test.sh
+++ regression_test.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-python --version
-bash /opt/tensorrt/python/python_setup.sh
-python utilities/bbox_setup.py build_ext --inplace && mv bbox_overlap.c* utilities/ && ls -lth
+python --version && \
+bash /opt/tensorrt/python/python_setup.sh && \
+python utilities/bbox_setup.py build_ext --inplace && mv bbox_overlap.c* utilities/ && ls -lth && \
 
 
 # yield model
-python run_inference.py --config config/yield/model_config_regression_yield_absolute_newview2.yaml --freeze_to_pb_path ~/zoomlion-sample/tmp_resnet_model --upload
-python run_inference.py --config config/yield/model_config_regression_yield_absolute_newview2.yaml --create_trt_engine --debug
+python run_inference.py --config config/yield/model_config_regression_yield_absolute_newview2.yaml --freeze_to_pb_path ~/zoomlion-sample/tmp_resnet_model --upload && \
+python run_inference.py --config config/yield/model_config_regression_yield_absolute_newview2.yaml --create_trt_engine --debug && \
 
 # chaff hopper
-python run_inference.py --config config/yield/model_config_regression_yield_absolute_newview2.yaml --freeze_to_pb_path ~/zoomlion-sample/tmp_unet_chaff_hopper_model --upload
-python run_inference.py --config config/chaff_hopper/model_config_segmentation_chaff.yaml --create_trt_engine --debug
+python run_inference.py --config config/chaff_hopper/model_config_segmentation_chaff.yaml --freeze_to_pb_path ~/zoomlion-sample/tmp_unet_chaff_hopper_model --upload && \
+python run_inference.py --config config/chaff_hopper/model_config_segmentation_chaff.yaml --create_trt_engine --debug && \
 
 # lodging
-python run_inference.py --config config/lodging/model_config_segmentation_lodging.yaml --freeze_to_pb_path ~/zoomlion-sample/tmp_unet_lodging_model --upload
-python run_inference.py --config config/lodging/model_config_segmentation_lodging.yaml --create_trt_engine --debug
+python run_inference.py --config config/lodging/model_config_segmentation_lodging.yaml --freeze_to_pb_path ~/zoomlion-sample/tmp_unet_lodging_model --upload && \
+python run_inference.py --config config/lodging/model_config_segmentation_lodging.yaml --create_trt_engine --debug && \
 
 # chaff elevator
-python run_inference.py --config config/chaff_elevator/model_config_segmentation_chaff.yaml --freeze_to_pb_path ~/zoomlion-sample/tmp_unet_chaff_elevator_model --upload
+python run_inference.py --config config/chaff_elevator/model_config_segmentation_chaff.yaml --freeze_to_pb_path ~/zoomlion-sample/tmp_unet_chaff_elevator_model --upload && \
 python run_inference.py --config config/chaff_elevator/model_config_segmentation_chaff.yaml --create_trt_engine --debug
