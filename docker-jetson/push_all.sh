43ad2596bc3f479448d04c49ffaec4b3ff58eed8
yuanzhedong <yuanzhedong@gmail.com>
2020-02-02 05:41:11 +0000

    update scripts

--- docker-jetson/push_all.sh
+++ docker-jetson/push_all.sh
@@ -15,15 +15,15 @@ $login
 
 aws s3 sync s3://zoomlion-prod-data/xavier/debs ./debs
 
-#mv ./debs/libnvinfer* ./nvinfer/
-#mv ./debs/python3-libnvinfer* ./nvinfer/
-#mv ./debs/tensorrt* ./tensorrt/
-#mv ./debs/graphsurgeon* ./tensorrt/
-#mv ./debs/uff-converter* ./tensorrt/
-mv ./debs/libcudnn* ./cudnn/
-#mv ./debs/cuda*.zip ./opencv/
+cp ./debs/libnvinfer* ./nvinfer/
+cp ./debs/python3-libnvinfer* ./nvinfer/
+cp ./debs/tensorrt* ./tensorrt/
+cp ./debs/graphsurgeon* ./tensorrt/
+cp ./debs/uff-converter* ./tensorrt/
+cp ./debs/libcudnn* ./cudnn/
+cp ./debs/cuda*.zip ./opencv/
 #mv ./debs/cuda*.patch ./opencv/
-mv ./debs/cuda* ./cuda/
+cp ./debs/cuda* ./cuda/
 
 cd cuda
 #podman build -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static -t 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-xavier-l4t-cuda-dev .
@@ -38,6 +38,21 @@ podman build -v $(pwd)/../qemu-aarch64-static:/usr/bin/qemu-aarch64-static -t 28
 podman push 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-xavier-l4t-cuda-cudnn-dev
 cd ..
 
+
+cd nvinfer
+podman build -v $(pwd)/../qemu-aarch64-static:/usr/bin/qemu-aarch64-static -t 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-xavier-l4t-cuda-cudnn-nvinfer-dev .
+podman push 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-xavier-l4t-cuda-cudnn-nvinfer-dev
+cd ..
+
+cd tensorrt
+podman build -v $(pwd)/../qemu-aarch64-static:/usr/bin/qemu-aarch64-static -t 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-xavier-l4t-cuda-cudnn-nvinfer-tensorrt-dev .
+podman push 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-xavier-l4t-cuda-cudnn-nvinfer-tensorrt-dev
+cd ..
+
+cd opencv
+podman build -v $(pwd)/../qemu-aarch64-static:/usr/bin/qemu-aarch64-static -t 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-xavier-l4t-cuda-cudnn-nvinfer-tensorrt-opencvdev .
+podman push 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-xavier-l4t-cuda-cudnn-nvinfer-tensorrt-opencv-dev
+cd ..
 # cd tensorflow
 # podman build -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static -t 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-tensorflow-1.14.0 .
 # podman push 286751717145.dkr.ecr.us-east-2.amazonaws.com/zoomlion:jetson-tensorflow-1.14.0
