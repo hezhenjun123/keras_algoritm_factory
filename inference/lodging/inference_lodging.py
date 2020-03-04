609e937996d84a11d7da8f30c3f940837fd0d1d7
weifoo <wfu@ncsu.edu>
2020-03-04 22:57:33 +0000

    make binary classification and report metrics

--- inference/lodging/inference_lodging.py
+++ inference/lodging/inference_lodging.py
@@ -18,6 +18,7 @@ class InferenceLodging(InferenceBase):
         self.pred_image_dir = config["INFERENCE"]["PRED_IMAGE_DIR"]
         self.num_process_image = config["INFERENCE"]["NUM_PROCESS_IMAGE"]
         self.evaluate = config["INFERENCE"]["EVALUATE"]
+        self.inference_visualize = config["INFERENCE"]["VISUALIZE"]
         self.contour_threshold = config["INFERENCE"]["CONTOUR_AREA_THRESHOLD"]
         if self.num_process_image >= 99999:
             raise Exception("cannot process more than 99999 images ")
@@ -30,7 +31,7 @@ class InferenceLodging(InferenceBase):
         if self.evaluate:
             self.__produce_evaluation(model, inference_dataset)
         else:
-            self.__produce_segmentation_image(model, inference_dataset)
+            self.__inference(model, inference_dataset)
         logging.info("================Inference Complete=============")
 
     def __produce_evaluation(self, model, dataset):
@@ -38,46 +39,65 @@ class InferenceLodging(InferenceBase):
         model.evaluate(dataset)
         logging.info("================Evaluation Completes=============")
         
-    def __produce_segmentation_image(self, model, dataset):
+    def __inference(self, model, dataset):
+        """
+        Given model and inference data set in format of (resize_img, original_img, resize_seg_label, original_seg_label), 
+        make binary classification and return the predictions. 
+        """
         
-        colors = {"labels":(0,255,0), "predicted":(0,0,255)}
-        def process_contours(original_image, seg_map, image_name="predicted"):
-            contours, _ = cv2.findContours(seg_map, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-            newimg = np.copy(original_image)
-            res_flag = False
-            for idx, contour in enumerate(contours):
-                area = cv2.contourArea(contour)
-                if area < self.contour_threshold:
-                    continue 
-                else:
-                    res_flag = True
-                print(f"predicted area {idx}", area)
-                cv2.drawContours(newimg, contour, -1, colors[image_name], 2)
-            if image_name == "predicted":
-                predicted.append(1 if res_flag else 0)
-            else:
-                gt.append(1 if res_flag else 0)
-            contour_dir = os.path.join(save_dir_contour,
-                                                f"image{count:05d}_{image_name}.png")
-            image = cv2.putText(newimg, image_name, (500, 500), cv2.FONT_HERSHEY_SIMPLEX , 1, colors[image_name], 2, cv2.LINE_AA) 
-            cv2.imwrite(contour_dir, newimg)
+        def process_contours(original_image, seg_maps):
+            """
+            Given the original image, and the pair of ground_truth_seg and prediction_seg_map, 
+            generate contours and make binary classification.
 
-        inference_dataset = dataset
-        save_dir_model = os.path.join(self.save_dir, self.pred_image_dir, "model")
-        save_dir_contour = os.path.join(self.save_dir, self.pred_image_dir,
-                                                 "contour")
-        save_dir_original = os.path.join(self.save_dir, self.pred_image_dir, "original")
-        save_dir_segmap = os.path.join(self.save_dir, self.pred_image_dir, "predicted-segmap")
+            Arguments:
+                original_image {np.array} -- the original image.
+                seg_maps {list} -- [ground_truth_seg_map, predicted_seg_map]
 
-        os.makedirs(save_dir_model, exist_ok=True)
-        os.makedirs(save_dir_contour, exist_ok=True)
-        os.makedirs(save_dir_original, exist_ok=True)
-        os.makedirs(save_dir_segmap, exist_ok=True)
+            Returns:
+                [list] -- [ground truth, prediction]
+            """
+            colors = [(0,255,0), (0,0,255)]
+            seg_names = ["Ground_truth", "Prediction"]
+            res = []
+            img_to_draw = []
+            for idx, seg_map in enumerate(seg_maps):
+                newimg = np.copy(original_image)
+                contours, _ = cv2.findContours(seg_map, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                res_flag = False
+                for i, contour in enumerate(contours):
+                    area = cv2.contourArea(contour)
+                    if area < self.contour_threshold:
+                        continue 
+                    else:
+                        res_flag = True
+                    logging.info(f"{seg_names[idx]} area {i}: {area}")
+
+                    if self.inference_visualize:
+                        cv2.drawContours(newimg, contour, -1, colors[idx], 2)
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        loc = (500, 500)
+                        newimg = cv2.putText(newimg, seg_names[idx], loc, font, 1, colors[idx], 2, cv2.LINE_AA) 
+                
+                res.append(1 if res_flag else 0) # 1 NG; 0 OK.
+                img_to_draw.append(newimg)
+            
+            if self.inference_visualize:
+                img_name = f"image{count:05d}_groundtruth_and_prediction.png"
+                contour_dir = os.path.join(save_dir_contour,img_name)
+                cv2.imwrite(contour_dir, np.concatenate(img_to_draw, axis=1))
+            return res
+
+        if self.inference_visualize:
+            save_dir_contour = os.path.join(self.save_dir, self.pred_image_dir,
+                                                    "contour")
+            os.makedirs(save_dir_contour, exist_ok=True)
 
         count = 0
-        gt = []
-        predicted = []
+        gt, predicted = [], []
+        inference_dataset = dataset
         for elem in inference_dataset:
+            assert(len(elem)==4, "the inference data format is wrong")
             pred_res = model.predict(elem[0])
             transformed_image = np.squeeze(elem[0], axis=0)
             original_image = np.squeeze(elem[1], axis=0)
@@ -93,31 +113,25 @@ class InferenceLodging(InferenceBase):
                                           interpolation=cv2.INTER_NEAREST).astype(np.uint8)
 
             logging.info(f"processed image: {count:05d}")
-
+            
             original_image = original_image[:, :, ::-1]
-            original_dir = os.path.join(save_dir_original, f"image{count:05d}_original.png")
-            cv2.imwrite(original_dir, original_image)
-
-            ## predicted 
-            process_contours(original_image, resized_pred_seg, "predicted")
-
-            ## labels 
             original_labels = np.squeeze(elem[3], axis=0)
-            process_contours(original_image, original_labels, "labels")
-
-            segmap_dir = os.path.join(save_dir_segmap, f"image{count:05d}_segmap.png")
-            cv2.imwrite(segmap_dir, resized_pred_seg)
-
+            res = process_contours(original_image, [original_labels, resized_pred_seg])
+            gt.append(res[0])
+            predicted.append(res[1])
+            
             count += 1
             if count >= self.num_process_image: break
+
         gt = np.array(gt)
         predicted = np.array(predicted)
         acc = sum(gt == predicted)*1.0/len(gt)
         matrix = confusion_matrix(gt, predicted)
         report = classification_report(gt, predicted)
         wrong_predictions = np.where(predicted != gt)
-        print(f"Wrong predictions:{wrong_predictions[0]}")
-        print(f"Inferece Accuracy: {acc}")
-        print(f"Confusion Matrix:\n {matrix}")
-        print(f"Classification Report:\n {report}")
+        logging.info(f"Wrong prediction image indexes:{wrong_predictions[0]}")
+        logging.info(f"Inferece accuracy: {acc}")
+        logging.info(f"Confusion matrix:\n {matrix}")
+        logging.info(f"Classification report:\n {report}")
+        return predicted
 
